40edf7a17141e7505610f2b64098066abf65e6fa
syffer <maxime.nicolas2@hotmail.fr>
2015-06-12 17:01:41 +0200

    vue en construction

--- src/vue/Vue.java
+++ src/vue/Vue.java
@@ -1,6 +1,12 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import internationalisation.Constantes;
 
@@ -8,8 +14,17 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+
+import modele.Match;
 
 
 
@@ -17,77 +32,204 @@ public class Vue extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	// la barre de menu.
+	public JMenuItem menuNouveauFichier;
+	public JMenuItem menuOuvrirFichier;
+	public JMenuItem menuSauvegarder;
+	public JMenuItem menuAnnuler;
+	public JMenuItem menuRefaire;
+	
+	// le panneau des joueurs
+	public JTable tableauJoueurs;
 	public JTextField champAjoutJoueur;
 	public JButton boutonAjoutJoueur;
-	
 	public JButton boutonSupprimerJoueur;
 	
-	public JButton boutonAjouterMatch;
+	//
+	public JButton boutonCreerMatch;
 	public JButton boutonGenererMatchs;
-	public JButton boutonAnnulerMatchs;
+	public JButton boutonAnnulerLesMatchs;
+	
+	// le panneau des matchs
+	public JList<Match> listeMatchs;
+	public JButton boutonJoueur1Gagne;
+	public JButton boutonJoueur2Gagne;
+	public JButton boutonJoueur1Abandonne;
+	public JButton boutonJoueur2Abandonne;
+	public JButton boutonMatchNull;
+	public JButton boutonAnnulerMatch;
 	
 	
 	public Vue() {
 		super( Constantes.getString(Constantes.TITRE_FENETRE) );
 		
+		this.ajouterBarreDesMenus();
 		
-		JPanel panneauJoueurs = new JPanel();
-		panneauJoueurs.setLayout( new BoxLayout(panneauJoueurs, BoxLayout.Y_AXIS) );	// de haut en bas
-		panneauJoueurs.setBorder( BorderFactory.createEtchedBorder() );		// faire apparaitre le contour
-		this.add(panneauJoueurs);
+		this.ajouterPanneauJoueurs();
 		
+		this.ajouterPanneauIntermediaire();
 		
-		
-		
-		
-		// ajout du tableau des joueurs
-		
-		JPanel panneauAjoutJoueur = new JPanel();
-		panneauAjoutJoueur.setLayout( new FlowLayout() );	// de gauche à droite
-		panneauJoueurs.add(panneauAjoutJoueur);
-		
-		
-		this.champAjoutJoueur = new JTextField();
-		this.champAjoutJoueur.setColumns(5);
-		//panneauAjoutJoueur.add( this.champAjoutJoueur );
-		
-		this.boutonAjoutJoueur = new JButton("AJOUTER JOUEUR !!!!!");
-		panneauAjoutJoueur.add( this.boutonAjoutJoueur );
-		
-		
-		
-		
-		this.boutonSupprimerJoueur = new JButton("SUPPRIMER JOUEUR !!!");
-		panneauJoueurs.add( this.boutonSupprimerJoueur );
-		
-		
-		
-		
-		JPanel panneauBoutonsMatchs = new JPanel();
-		panneauBoutonsMatchs.setLayout( new BoxLayout(panneauBoutonsMatchs, BoxLayout.Y_AXIS) );	// de haut en bas
-		panneauBoutonsMatchs.setBorder( BorderFactory.createEtchedBorder() );		// faire apparaitre le contour
-		this.add(panneauBoutonsMatchs);
-		
-		
-		this.boutonAjouterMatch = new JButton("AJOUTER MATCH !!!!!");
-		panneauBoutonsMatchs.add( this.boutonAjouterMatch );
-		
-		this.boutonGenererMatchs = new JButton("GENERER MATCH ");
-		panneauBoutonsMatchs.add( this.boutonGenererMatchs );
-		
-		this.boutonAnnulerMatchs = new JButton("Annuler les MATCHS");
-		panneauBoutonsMatchs.add( this.boutonAnnulerMatchs );
-		
-		
-		
-		
+		this.ajouterPanneauMatchs();
 		
 		
 		this.setLayout( new FlowLayout() );		// de gauche à droite
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.pack();
+		this.setLocationRelativeTo(null);	// on place la fenetre au centre de l'écran
+		this.pack();		
+		
 	}
 
+	
+	private void ajouterBarreDesMenus() {
+		
+		JMenu menuFichier = new JMenu("Fichier");
+		this.menuNouveauFichier = new JMenuItem("Nouveau tournoi");
+		this.menuOuvrirFichier = new JMenuItem("Charger un tournoi");
+		this.menuSauvegarder = new JMenuItem("Sauvegarder le tournoi");
+		menuFichier.add( this.menuNouveauFichier );
+		menuFichier.add( this.menuOuvrirFichier );
+		menuFichier.add( this.menuSauvegarder );
+		
+		JMenu menuEditer = new JMenu("Editer");
+		this.menuAnnuler = new JMenuItem("Annuler");
+		this.menuRefaire = new JMenuItem("Refaire");
+		menuEditer.add( this.menuAnnuler );
+		menuEditer.add( this.menuRefaire );
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(menuFichier);
+		menuBar.add(menuEditer);
+		this.setJMenuBar(menuBar);
+		
+	}
+		
+	
+	private void ajouterPanneauJoueurs() {
+		
+		JPanel panneauJoueurs = new JPanel();
+		
+		Border contour = BorderFactory.createEtchedBorder();
+		TitledBorder titre = BorderFactory.createTitledBorder( contour, "Joueurs" );
+		panneauJoueurs.setBorder( titre );		// faire apparaitre le contour
+		
+		panneauJoueurs.setLayout( new GridBagLayout() );
+		GridBagConstraints contrainte = new GridBagConstraints();
+		//panneauJoueurs.setLayout( new BoxLayout(panneauJoueurs, BoxLayout.Y_AXIS) );	// de haut en bas
+		this.add(panneauJoueurs);
+		
+		
+		this.tableauJoueurs = new JTable();	
+		this.tableauJoueurs.setPreferredSize( new Dimension(300, 400) ); 	// ( largeur , hauteur )
+		this.tableauJoueurs.setSize( new Dimension(300, 400) );
+		this.donnerContrainte( contrainte, 0, 0, 1, 1, 100, 100 );
+		panneauJoueurs.add( this.tableauJoueurs, contrainte );
+		
+		
+		JPanel panneauAjoutJoueur = new JPanel();
+		panneauAjoutJoueur.setLayout( new FlowLayout() );
+		this.donnerContrainte( contrainte, 0, 1, 1, 1, 100, 100 );
+		panneauJoueurs.add( panneauAjoutJoueur, contrainte );
+		
+		this.boutonSupprimerJoueur = new JButton("SUPPRIMER JOUEUR");
+		this.donnerContrainte( contrainte, 0, 2, 1, 1, 100, 100 );
+		panneauJoueurs.add( this.boutonSupprimerJoueur, contrainte );
+		
+		
+		
+		this.champAjoutJoueur = new JTextField();
+		this.champAjoutJoueur.setColumns(10);
+		panneauAjoutJoueur.add( this.champAjoutJoueur );
+		
+		this.boutonAjoutJoueur = new JButton("AJOUT DUN JOUEUR");
+		panneauAjoutJoueur.add( this.boutonAjoutJoueur );
+			
+		
+	}
+	
+	
+	private void ajouterPanneauIntermediaire() {
+		
+		JPanel panneauIntermediaire = new JPanel();
+		panneauIntermediaire.setLayout( new GridLayout(3, 1, 0, 10) ); 	// grille de 3 lignes 1 colonne
+		this.add(panneauIntermediaire);
+		
+		this.boutonCreerMatch = new JButton("CREER UN MATCH");
+		panneauIntermediaire.add( this.boutonCreerMatch );
+		
+		this.boutonGenererMatchs = new JButton("GENERER LES MATCHS");
+		panneauIntermediaire.add( this.boutonGenererMatchs );
+		
+		this.boutonAnnulerLesMatchs = new JButton("ANNULER LES MATCHS");
+		panneauIntermediaire.add( this.boutonAnnulerLesMatchs );
+		
+	}
+	
+	private void ajouterPanneauMatchs() {
+		
+		JPanel panneauMatchs = new JPanel();
+		Border contour = BorderFactory.createEtchedBorder();
+		TitledBorder titre = BorderFactory.createTitledBorder( contour, "Matchs" );
+		panneauMatchs.setBorder( titre );		// faire apparaitre le contour
+		
+		//panneauMatchs.setLayout( new BoxLayout(panneauMatchs, BoxLayout.Y_AXIS) );
+		
+		panneauMatchs.setLayout( new BorderLayout() );
+		this.add(panneauMatchs);
+		
+		
+		JPanel panneauListeMatchs = new JPanel();
+		panneauMatchs.add( panneauListeMatchs, BorderLayout.WEST );
+		
+		
+		JPanel panneauBoutonsMatchs = new JPanel();
+		panneauBoutonsMatchs.setLayout( new GridLayout(6, 1, 0, 10) );	// grille de 6 lignes et 1 colonne
+		panneauMatchs.add( panneauBoutonsMatchs, BorderLayout.EAST );
+		
+		
+		
+		this.listeMatchs = new JList<Match>();
+		this.listeMatchs.setPreferredSize( new Dimension(200, 500) );
+		this.listeMatchs.setSize( new Dimension(200, 500) );
+		panneauListeMatchs.add( this.listeMatchs );
+		
+		this.boutonJoueur1Gagne = new JButton("J1 gagne");
+		panneauBoutonsMatchs.add( this.boutonJoueur1Gagne );
+		
+		this.boutonJoueur2Gagne = new JButton("J2 gagne");
+		panneauBoutonsMatchs.add( this.boutonJoueur2Gagne );
+		
+		this.boutonJoueur1Abandonne = new JButton("J1 abandonne");
+		panneauBoutonsMatchs.add( this.boutonJoueur1Abandonne );
+		
+		this.boutonJoueur2Abandonne = new JButton("J2 abandonne");
+		panneauBoutonsMatchs.add( this.boutonJoueur2Abandonne );
+		
+		this.boutonMatchNull = new JButton("null");
+		panneauBoutonsMatchs.add( this.boutonMatchNull );
+		
+		this.boutonAnnulerMatch = new JButton("annuler match");
+		panneauBoutonsMatchs.add( this.boutonAnnulerMatch );
+		
+	}
+	
+	
+	
+	private void donnerContrainte( GridBagConstraints gridBagContraints, int gx, int gy, int gw, int gh, int wx, int wy )  {
+		this.donnerContrainte( gridBagContraints, gx, gy, gw, gh, wx, wy, GridBagConstraints.BOTH );
+	}
+
+
+	private void donnerContrainte( GridBagConstraints gridBagContraints, int gx, int gy, int gw, int gh, int wx, int wy, int constraint ) {  
+		gridBagContraints.gridx = gx;
+		gridBagContraints.gridy = gy;
+		gridBagContraints.gridwidth = gw;
+		gridBagContraints.gridheight = gh;
+		gridBagContraints.weightx = wx;
+		gridBagContraints.weighty = wy;
+		gridBagContraints.fill = constraint;
+	}
+	
+	
 	
 	
 	public static class Test {
