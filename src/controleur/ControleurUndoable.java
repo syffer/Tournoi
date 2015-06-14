1a8ecd652b6343db7834bfac1b9414cdc42469f7
syffer <maxime.nicolas2@hotmail.fr>
2015-06-14 03:05:27 +0200

    actions undo / redo ( + leurs shortcuts )

--- src/controleur/ControleurUndoable.java
+++ src/controleur/ControleurUndoable.java
@@ -1,12 +1,18 @@
 package controleur;
 
+import internationalisation.Constantes;
+
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
 
 import vue.VueUndoable;
+import modele.ModeleUndoManager;
 import modele.ModeleUndoable;
 
 public class ControleurUndoable extends Controleur {
@@ -18,30 +24,40 @@ public class ControleurUndoable extends Controleur {
 		ActionUndo actionUndo = new ActionUndo();
 		ActionRedo actionRedo = new ActionRedo();
 		
+		VueUndoable vueUndoable = (VueUndoable) this.vue;
 		
+		vueUndoable.menuAnnuler.setAction(actionUndo);
+		vueUndoable.menuRefaire.setAction(actionRedo);
 		
-		
+		modeleUndoable.getModeleUndoManager().initialiser();
 	}
 	
 	
 	public class ActionUndo extends AbstractAction implements Observer {
 		
-		private static final long serialVersionUID = -3578057936698716615L;
-
+		private static final long serialVersionUID = -3573057936698716615L;
+		
+		private ModeleUndoManager undoManager;
+		
 		public ActionUndo() {
+			super( Constantes.getString(Constantes.UNDO) );
 			
+			ModeleUndoable modeleUndoable = (ModeleUndoable) ControleurUndoable.this.modele;
+			this.undoManager = modeleUndoable.getModeleUndoManager();
+			this.undoManager.addObserver(this);
+			
+			this.putValue( AbstractAction.NAME, Constantes.getString(Constantes.UNDO) );
+			this.putValue( AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK) ); 	// Ctrl + S
 		}
 		
 		@Override
 		public void actionPerformed( ActionEvent event ) {
-			// TODO Auto-generated method stub
-			
+			this.undoManager.undo();
 		}
 
 		@Override
 		public void update( Observable observable, Object args ) {
-			// TODO Auto-generated method stub
-			
+			this.setEnabled( this.undoManager.canUndo() );
 		}
 		
 	}
@@ -51,20 +67,27 @@ public class ControleurUndoable extends Controleur {
 		
 		private static final long serialVersionUID = -3578057936608716615L;
 
+		private ModeleUndoManager undoManager;
+		
 		public ActionRedo() {
+			super( Constantes.getString(Constantes.REDO) );
 			
+			ModeleUndoable modeleUndoable = (ModeleUndoable) ControleurUndoable.this.modele;
+			this.undoManager = modeleUndoable.getModeleUndoManager();
+			this.undoManager.addObserver(this);		
+									
+			this.putValue( AbstractAction.NAME, Constantes.getString(Constantes.REDO) );
+			this.putValue( AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_Y, KeyEvent.CTRL_DOWN_MASK) ); 	// Ctrl + Y
 		}
 		
 		@Override
 		public void actionPerformed( ActionEvent event ) {
-			// TODO Auto-generated method stub
-			
+			this.undoManager.redo();
 		}
 
 		@Override
 		public void update( Observable observable, Object args ) {
-			// TODO Auto-generated method stub
-			
+			this.setEnabled( this.undoManager.canRedo() );
 		}
 		
 	}
